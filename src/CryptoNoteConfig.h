b2b2c4e8847d22a615ac61e9926f7125c778a3b5
wrkzdev <40448869+wrkzdev@users.noreply.github.com>
2018-09-18 22:34:29 +0700

    Mixing

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -78,9 +78,9 @@ const uint32_t MIXIN_LIMITS_V3_HEIGHT                        = 600000;
 /* The mixin to use by default with zedwallet and wrkz-service */
 /* DEFAULT_MIXIN_V0 is the mixin used before MIXIN_LIMITS_V1_HEIGHT is started */
 const uint64_t DEFAULT_MIXIN_V0                              = 3;
-const uint64_t DEFAULT_MIXIN_V1                              = MAXIMUM_MIXIN_V1;
-const uint64_t DEFAULT_MIXIN_V2                              = MAXIMUM_MIXIN_V2;
-const uint64_t DEFAULT_MIXIN_V3                              = MAXIMUM_MIXIN_V3;
+const uint64_t DEFAULT_MIXIN_V1                              = MINIMUM_MIXIN_V2;
+const uint64_t DEFAULT_MIXIN_V2                              = MINIMUM_MIXIN_V2;
+const uint64_t DEFAULT_MIXIN_V3                              = MINIMUM_MIXIN_V3;
 
 const uint64_t DEFAULT_DUST_THRESHOLD                        = UINT64_C(10);
 const uint64_t DEFAULT_DUST_THRESHOLD_V2                     = UINT64_C(0);
