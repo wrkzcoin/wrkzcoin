33680325b1d6290f36b07d42b92aec6a6c7852ef
wrkzdev <40448869+wrkzdev@users.noreply.github.com>
2019-04-01 08:28:37 +0700

    Internal checkpoints till 397,000

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -123,7 +123,7 @@ const uint64_t MAX_EXTRA_SIZE_V2_HEIGHT                      = 494800;
 
 /* For new projects forked from this code base, this value should be
    changed to 0 to prevent a possible transaction bloat exploit */
-const uint64_t TRANSACTION_SIGNATURE_COUNT_VALIDATION_HEIGHT = 1400000;
+const uint64_t TRANSACTION_SIGNATURE_COUNT_VALIDATION_HEIGHT = 494800;
 
 const uint64_t CRYPTONOTE_LOCKED_TX_ALLOWED_DELTA_BLOCKS     = 1;
 const uint64_t CRYPTONOTE_LOCKED_TX_ALLOWED_DELTA_SECONDS    = DIFFICULTY_TARGET * CRYPTONOTE_LOCKED_TX_ALLOWED_DELTA_BLOCKS;
