cf12c9f9f008f69c00e520a77d5ad3c8bae29f3b
wrkzdev <40448869+wrkzdev@users.noreply.github.com>
2019-10-10 13:07:34 +0700

    Update P2P versions and fork index

--- src/config/CryptoNoteConfig.h
+++ src/config/CryptoNoteConfig.h
@@ -249,7 +249,7 @@ namespace CryptoNote
         };
 
         /* MAKE SURE TO UPDATE THIS VALUE WITH EVERY MAJOR RELEASE BEFORE A FORK */
-        const uint64_t SOFTWARE_SUPPORTED_FORK_INDEX = 6;
+        const uint64_t SOFTWARE_SUPPORTED_FORK_INDEX = 7;
 
         const uint64_t FORK_HEIGHTS_SIZE = sizeof(FORK_HEIGHTS) / sizeof(*FORK_HEIGHTS);
 
@@ -322,9 +322,9 @@ namespace CryptoNote
 
     // P2P Network Configuration Section - This defines our current P2P network version
     // and the minimum version for communication between nodes
-    const uint8_t P2P_CURRENT_VERSION = 5;
+    const uint8_t P2P_CURRENT_VERSION = 6;
 
-    const uint8_t P2P_MINIMUM_VERSION = 4;
+    const uint8_t P2P_MINIMUM_VERSION = 5;
 
     // This defines the minimum P2P version required for lite blocks propogation
     const uint8_t P2P_LITE_BLOCKS_PROPOGATION_VERSION = 4;
