0a3393abe7f8c17baf43825bcff9231afb0ec90a
wrkzdev <40448869+wrkzdev@users.noreply.github.com>
2019-12-31 07:49:02 +0700

    Fix wallet-service log levels not working as expected turtlecoin#983

--- src/walletservice/PaymentGateService.cpp
+++ src/walletservice/PaymentGateService.cpp
@@ -64,6 +64,9 @@ bool PaymentGateService::init(int argc, char **argv)
     }
 
     logger->setMaxLevel(static_cast<Logging::Level>(config.serviceConfig.logLevel));
+    consoleLogger.setMaxLevel(static_cast<Logging::Level>(config.serviceConfig.logLevel));
+    fileLogger.setMaxLevel(static_cast<Logging::Level>(config.serviceConfig.logLevel));
+
     logger->setPattern("%D %T %L ");
     logger->addLogger(consoleLogger);
 
