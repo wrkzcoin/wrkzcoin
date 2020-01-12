dddd4db9ae17377108d3ff2ff23e40e3e71edfd4
wrkzdev <40448869+wrkzdev@users.noreply.github.com>
2020-01-12 07:06:41 +0700

    Update node myexplorer.wrkz.work

--- src/config/CryptoNoteConfig.h
+++ src/config/CryptoNoteConfig.h
@@ -395,7 +395,7 @@ namespace CryptoNote
 
     const char *const SEED_NODES[] = {
         "139.99.47.188:17855",        // node-sg1.wrkz.work
-        "95.216.164.177:17855",       // myexplorer.wrkz.work
+        "46.4.243.14:17855",          // myexplorer.wrkz.work
         "5.9.65.105:17855",           // node-eu1.wrkz.work
         "5.172.219.174:17855"         // wrkz.stx.nl sniperviperman
     };
