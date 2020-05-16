d25fcc3e0c7c3626508e18af0e475c741ec69eaa
wrkzdev <40448869+wrkzdev@users.noreply.github.com>
2020-05-17 06:42:12 +0700

    Update myexplorer.wrkz.work IP

--- src/config/CryptoNoteConfig.h
+++ src/config/CryptoNoteConfig.h
@@ -408,7 +408,7 @@ namespace CryptoNote
         {0xb5, 0x0c, 0x4a, 0x6c, 0xcf, 0x52, 0x57, 0x41, 0x65, 0xf9, 0x91, 0xa4, 0xb6, 0xc1, 0x43, 0xe9}};
 
     const char *const SEED_NODES[] = {
-        "176.9.99.123:17855",         // myexplorer.wrkz.work
+        "176.9.145.124:17855",        // myexplorer.wrkz.work
         "94.113.119.122:17855",       // publicnode.ydns.eu
         "178.238.236.173:17855"       // wrkz.xyz
     };
