ed9eb4e4f619cbf3013b53776fa022c1f44d3c5d
wrkzdev <40448869+wrkzdev@users.noreply.github.com>
2020-09-01 17:00:27 +0700

    Update p2p

--- src/config/CryptoNoteConfig.h
+++ src/config/CryptoNoteConfig.h
@@ -412,9 +412,9 @@ namespace CryptoNote
 
     // P2P Network Configuration Section - This defines our current P2P network version
     // and the minimum version for communication between nodes
-    const uint8_t P2P_CURRENT_VERSION = 14;
+    const uint8_t P2P_CURRENT_VERSION = 15;
 
-    const uint8_t P2P_MINIMUM_VERSION = 13;
+    const uint8_t P2P_MINIMUM_VERSION = 14;
 
     // This defines the minimum P2P version required for lite blocks propogation
     const uint8_t P2P_LITE_BLOCKS_PROPOGATION_VERSION = 4;
