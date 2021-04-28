f7bba02ce1a7a21a9bc840a67e72efdf5a844aa9
wrkzdev <40448869+wrkzdev@users.noreply.github.com>
2021-04-28 09:56:48 +0700

    Remove arm-node.wrkz.work

--- src/config/CryptoNoteConfig.h
+++ src/config/CryptoNoteConfig.h
@@ -478,8 +478,7 @@ namespace CryptoNote
         {0xb5, 0x0c, 0x4a, 0x6c, 0xcf, 0x52, 0x57, 0x41, 0x65, 0xf9, 0x91, 0xa4, 0xb6, 0xc1, 0x43, 0xe9}};
 
     const char *const SEED_NODES[] = {
-        "95.216.115.140:17855",        // myexplorer.wrkz.work
-        "51.15.243.43:17855",         // arm-node.wrkz.work
+        "95.216.115.140:17855",       // myexplorer.wrkz.work
         "95.217.121.35:17855",        // node-fin.wrkz.work
         "94.113.119.122:17855",       // publicnode.ydns.eu
         "178.238.236.173:17855"       // wrkz.xyz
