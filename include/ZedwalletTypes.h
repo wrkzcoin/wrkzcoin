2616edf4cd4cde2d4279b41173ef798cd2c61bc3
pluton <wrkzdev@gmail.com>
2026-02-21 04:31:05 +0700

    Remove unused WalletInfo constructor and Maybe helpers from ZedwalletTypes

--- include/ZedwalletTypes.h
+++ include/ZedwalletTypes.h
@@ -11,20 +11,6 @@
 
 struct WalletInfo
 {
-    WalletInfo(
-        std::string walletFileName,
-        std::string walletPass,
-        std::string walletAddress,
-        bool viewWallet,
-        CryptoNote::WalletGreen &wallet):
-        walletFileName(walletFileName),
-        walletPass(walletPass),
-        walletAddress(walletAddress),
-        viewWallet(viewWallet),
-        wallet(wallet)
-    {
-    }
-
     /* How many transactions do we know about */
     size_t knownTransactionCount = 0;
 
@@ -112,64 +98,6 @@ struct AddressBookEntry
     }
 };
 
-/* An address book is a vector of address book entries */
-typedef std::vector<AddressBookEntry> AddressBook;
-
-/* This borrows from haskell, and is a nicer boost::optional class. We either
-   have Just a value, or Nothing.
-
-   Example usage follows.
-   The below code will print:
-
-   ```
-   100
-   Nothing
-   ```
-
-   Maybe<int> parseAmount(std::string input)
-   {
-        if (input.length() == 0)
-        {
-            return Nothing<int>();
-        }
-
-        try
-        {
-            return Just<int>(std::stoi(input)
-        }
-        catch (const std::invalid_argument &)
-        {
-            return Nothing<int>();
-        }
-   }
-
-   int main()
-   {
-       auto foo = parseAmount("100");
-
-       if (foo.isJust)
-       {
-           std::cout << foo.x << std::endl;
-       }
-       else
-       {
-           std::cout << "Nothing" << std::endl;
-       }
-
-       auto bar = parseAmount("garbage");
-
-       if (bar.isJust)
-       {
-           std::cout << bar.x << std::endl;
-       }
-       else
-       {
-           std::cout << "Nothing" << std::endl;
-       }
-   }
-
-*/
-
 template<class X> struct Maybe
 {
     X x;
@@ -180,13 +108,3 @@ template<class X> struct Maybe
 
     Maybe(): isJust(false) {}
 };
-
-template<class X> Maybe<X> Just(const X &x)
-{
-    return Maybe<X>(x);
-}
-
-template<class X> Maybe<X> Nothing()
-{
-    return Maybe<X>();
-}
